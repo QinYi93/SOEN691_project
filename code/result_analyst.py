bc1fcfbccc924fdce6497dd5f56a44461a41f8b3
sakichat <saki28chat@gmail.com>
2018-04-17 18:34:08 -0400

    result_analyst

---
+++ code/result_analyst.py
@@ -0,0 +1,13 @@
+from pyspark import Row
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("result_analyst").getOrCreate()
+item_result = spark.read.csv('./results/item_based_prediciton.csv').rdd
+itemRDD = item_result.map(lambda p: Row(userId=int(p[0]), movieId=int(p[1]), ratings=float(p[2])))
+itemRatings = spark.createDataFrame(itemRDD)
+itemRatings = itemRatings.sort(['userId', 'ratings'], ascending=False).show(200)
+
+user_result = spark.read.csv('./results/user_based_prediciton.csv').rdd
+userRDD = user_result.map(lambda p: Row(userId=int(p[0]), movieId=int(p[1]), ratings=float(p[2])))
+userRatings = spark.createDataFrame(userRDD)
+userRatings = userRatings.sort(['userId', 'ratings'], ascending=False).show(200)
